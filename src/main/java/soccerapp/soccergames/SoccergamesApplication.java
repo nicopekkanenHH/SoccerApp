29a2d4b68fccc1bf6aac436473a18bc37bfa3073
nicopekkanenHH <165182826+nicopekkanenHH@users.noreply.github.com>
2024-10-14 19:36:41 +0300

    Resolved merge conflicts

---
+++ src/main/java/soccerapp/soccergames/SoccergamesApplication.java
@@ -0,0 +1,13 @@
+package soccerapp.soccergames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccergamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoccergamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/soccerapp/soccergames/SoccergamesApplicationTests.java
@@ -0,0 +1,13 @@
+package soccerapp.soccergames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoccergamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
